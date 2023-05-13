891212abc2462276fe8312835fff00f249ea79d4
zhyunk <gimwlgus@gmail.com>
2023-05-14 03:31:51 +0900

    Mockito : db 연결 및 데이터 관리를 Mockito로 해보고 싶었는데 실패

--- java/src/test/java/mockito/DBTest.java
+++ java/src/test/java/mockito/DBTest.java
@@ -16,8 +16,8 @@ import static org.mockito.Mockito.verify;
 
 @DisplayName("DB Test")
 @ExtendWith(MockitoExtension.class)
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class MockitoDBTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DBTest {
     Connection conn;
     Statement stmt;
 
@@ -28,35 +28,22 @@ public class MockitoDBTest {
     UserService userService;
 
     @Test
-    void insert() {
-        User user = new User(1, "몽키", "monkey@zh.kim");
+    void data() throws SQLException {
+        stmt.execute("INSERT INTO USERS(NAME, EMAIL) VALUES('몽키', 'monkey@zh.kim');");
+        stmt.execute("INSERT INTO USERS(NAME, EMAIL) VALUES('코알라', 'native.bear@zh.kim');");
 
-        given( userRepository.findById(1) ).willReturn( Optional.of(user) );
-
-        User result = userService.getUserById(1);
-
-        assertEquals( user, result );
-        verify( userRepository, times(1) ).findById(1);
-
-        System.out.println(result.toString());
-    }
-    @Test
-    void insert2() throws SQLException {
-        User user = new User(2, "코알라", "native.bear@zh.kim");
-
-        given( userRepository.findById(1) ).willReturn( Optional.of(user) );
-
-        User result = userService.getUserById(1);
-
-        assertEquals(user, result);
-        verify( userRepository, times(1) ).findById(1);
-
-        System.out.println(result.toString());
+        // 데이터 조회
+        ResultSet rs = stmt.executeQuery("SELECT * FROM USERS;");
+        while (rs.next()) {
+            System.out.println(
+                    rs.getInt("ID") + ", " +
+                    rs.getString("NAME") + ", " +
+                    rs.getString("EMAIL")
+            );
+        }
     }
 
-
-    // 아래 2개의 메서드는 실제 값 넣어보는 용도로만 확인! 확인 시 class 위에 @TestInstance 주석 해제
-    // @BeforeAll
+    @BeforeAll
     void init() {
         try {
             Class.forName("org.h2.Driver");
@@ -78,7 +65,7 @@ public class MockitoDBTest {
         }
     }
 
-    // @AfterAll
+    @AfterAll
     void destroy() {
         try {
             stmt.execute(
